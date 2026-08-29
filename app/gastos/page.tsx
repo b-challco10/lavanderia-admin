6ae04e1b7b8e6098e2da704f5dd61c7312879bfc
brayan <brayan.challco.ticona2@gmail.com>
2026-08-29 17:30:26 -0400

    arreglo orden

--- app/gastos/page.tsx
+++ app/gastos/page.tsx
@@ -6,7 +6,7 @@ export const dynamic = "force-dynamic";
 export default async function GastosPage() {
   const gastos = await prisma.gasto.findMany({
     orderBy: {
-      fecha: "desc",
+      createdAt: "desc",
     },
   });
 
