e75e93d0d22b5889bc8bc91df941cab97cd4a35e
brayan <brayan.challco.ticona2@gmail.com>
2026-09-11 20:03:36 -0400

    arreglo

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
 import { redirect } from "next/navigation";
+import { obtenerSesion } from "@/lib/auth";
 
-export default function Home() {
+export default async function Home() {
+  const sesion = await obtenerSesion();
+
+  if (!sesion) {
+    redirect("/login");
+  }
+
+  if (!sesion.sucursalId) {
+    redirect("/seleccionar-sucursal");
+  }
+
+  if (sesion.rol === "EMPLEADO") {
+    redirect("/pedidos");
+  }
+
   redirect("/dashboard");
 }
